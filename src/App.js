47bc250a8c6aa3df0c06c432f6905e686f2075b9
Edson <edson_a@outlook.com>
2023-04-12 17:25:55 -0300

    aula-15

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navigation from './components/Navigation';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Hero />
       <h2>Edson</h2>
     </div>
   );
